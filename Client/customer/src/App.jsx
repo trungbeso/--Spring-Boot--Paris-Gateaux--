29dbcddf456540c19941a7dbde640dccaa879b94
Benjamin <Benjamin@gmail.com>
2025-04-14 11:03:53 +0700

    Refactor components and update dependencies; add Header and Home components with carousel functionality

--- Client/customer/src/App.jsx
+++ Client/customer/src/App.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from "react";
 import Menubar from "./components/Menubar.jsx";
-import Home from './pages/Home'
+import Home from "./pages/Home.jsx";
 import ExploreFood from "./pages/ExploreFood.jsx";
-import Contact from './pages/Contact.jsx'
-import { Route, Routes} from "react-router-dom";
-import FoodDetails from './pages/FoodDetails.jsx'
-import Cart from "./pages/Cart.jsx"
+import Contact from "./pages/Contact.jsx";
+import { Route, Routes } from "react-router-dom";
+import FoodDetails from "./pages/FoodDetails.jsx";
+import Cart from "./pages/Cart.jsx";
 import PlaceOrder from "./pages/PlaceOrder.jsx";
 
 const App = () => {
   return (
-   <>
-       <Menubar />
-       <Routes>
-           <Route path="/" element={<Home />}/>
-           <Route path="/explore" element={<ExploreFood />}/>
-           <Route path="/contact" element={<Contact />}/>
-           <Route path="/food/:id" element={<FoodDetails />}/>
-           <Route path="/cart" element={<Cart/>}/>
-           <Route path="/order" element={<PlaceOrder />}/>
-       </Routes>
-   </>
+    <>
+      <Menubar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<ExploreFood />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/food/:id" element={<FoodDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
+    </>
+  );
+};
 
-
-  )
-}
-
-export default App
+export default App;
